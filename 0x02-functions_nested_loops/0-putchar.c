77599aeefbcbc065187596bc094f16364d61a595
Chukwudi Egoebichi <chuksegoebichi.com>
2023-02-20 23:34:55 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print _putchar thrn a new line
+ * Return:0 Always (SUCCESS)
+ * betty style doc for function main goes there
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
