8ab21736349e204339f0dc49cd51ddea3d81054f
Aristotle1m <123338707+Aristotle1m@users.noreply.github.com>
2023-02-21 06:34:14 -0800

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,12 @@
 #include "main.h"
 /**
  * print_alphabet - Entry point
- *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	int alpha;
-
 	alpha = 'a';
-
 	while (alpha <= 'z')
 	{
 		_putchar(alpha);
