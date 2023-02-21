1db875f7c47387d99f9ebf4da6b07685d53e19f9
Aristotle1m <123338707+Aristotle1m@users.noreply.github.com>
2023-02-21 07:06:14 -0800

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet- This is where the execution starts
- * Function prints the alphabet in lower case
+ * print alphabet - This is where the execution starts
+ * Function prints the alphabet in lowercase
  * followed by a new line
- * Return: Always 0 (Success)
+ * Return:0 (Success)
  */
 void print_alphabet(void)
 {
 char letter = 'a';
 while (letter <= 'z')
-	{
+{
 _putchar(letter);
 letter++;
-	}
+}
 _putchar('\n');
 }
