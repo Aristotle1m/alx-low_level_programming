8f80022741a15da672e21de0707339ebd24949fe
Chukwudi Egoebichi <chuksegoebichi.com>
2023-02-27 04:01:46 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes input to pointer and updates the values
+ * @x: int pointer
+ * Return: void
+ */
+
+void reset_to_98(int *x)
+{
+	*x = 98;
+}
