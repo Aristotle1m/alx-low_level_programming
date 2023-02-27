68ac1d180b5b2af6ce5b50e437f08e2a403eff6e
Chukwudi Egoebichi <chuksegoebichi.com>
2023-02-27 04:09:31 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap values of two integers
+ * @x: 1st integer
+ * @y: 2nd integer
+ * Return: void
+ */
+
+void swap_int(int *x, int *y)
+{
+	int z = *a;
+
+	*x = *y;
+	*y = z;
+}
