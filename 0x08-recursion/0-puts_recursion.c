3fb9577bdb4e71601040a6e8a04d8aacebd55f6b
Chukwudi Egoebichi <chuksegoebichi.com>
2023-03-06 22:27:58 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- * _puts_recursion - a function that prints a string, followed by a new line.
- * @s: as parameter
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ * betty style doc for function main goes there
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
 }
+else
+_putchar('\n');
+}
+
