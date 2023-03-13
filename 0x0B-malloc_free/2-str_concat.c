12870a42ecb623516d487e6e9d91c53264168c86
Chukwudi Egoebichi <chuksegoebichi.com>
2023-03-13 00:39:19 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * str_concat - function which concatenates two strings.
- * @s1: string 1
- * @s2: string 2
- * Return: pointer of an array of chars
- */
+ * str_concat - concatenates two strings
+ * @s1: string to be concatenated on
+ * @s2: string to concatenate to s1
+ * Return: NULL if concatenate fails else a pointer to the
+ * newly allocated space in the memory
+*/
 
 char *str_concat(char *s1, char *s2)
 {
-	char *c_str;
-	int x, cx = 0,  l = 0;
+	char *cs;
+	int xc, cx = 0, lc = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -18,19 +20,19 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (x = 0; s1[x] || s2[x]; x++)
-		l++;
+	for (xc = 0; s1[xc] || s2[xc]; xc++)
+		lc++;
 
-	c_str = malloc(sizeof(char) * l);
+	cs = malloc(sizeof(char) * lc);
 
-	if (c_str == NULL)
+	if (cs == NULL)
 		return (NULL);
 
-	for (x = 0; s1[x]; x++)
-		c_str[cx++] = s1[x];
+	for (xc = 0; s1[xc]; xc++)
+		cs[cx++] = s1[xc];
 
-	for (x = 0; s2[x]; x++)
-		c_str[cx++] = s2[x];
+	for (xc = 0; s2[xc]; xc++)
+		cs[cx++] = s2[xc];
 
-	return (c_str);
+	return (cs);
 }
