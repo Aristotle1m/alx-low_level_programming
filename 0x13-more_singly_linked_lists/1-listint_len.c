aed45c6e280895b5e4a8788457570df8f0efa4d9
Chukwudi Egoebichi <chuksegoebichi.com>
2023-04-04 02:40:46 -0700

    good it is

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 
 /**
  * listint_len - return # of elements
- * @p: pointer to the first element
+ * @p: pointer to the first element.
  * Return: number of elements
  */
 
@@ -10,7 +10,7 @@ size_t listint_len(const listint_t *p)
 {
 	int i;
 
-	for (i = 0; h; p = (*p).next, i++)
+	for (i = 0; p; p = (*p).next, i++)
 		;
 
 	return (i);
--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,8 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		delete = (*hold).next;
 		(*hold).next = (*delete).next;
-		free(delete);
-		return (1);
+		free(delete);	return (1);
 	}
 	else if (!index && (**head).next)
 	{
