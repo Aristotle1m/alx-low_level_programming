14dbd54273388b5fea868ca5ff78d9c0f68e33fd
Chukwudi Egoebichi <chuksegoebichi.com>
2023-04-04 02:13:10 -0700

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * free_listint2 - function that frees listint_t list
+ * free_listint2 - function that frees the listint_t list
  * @head: double pointer to the beginning of the linked list
  */
 
