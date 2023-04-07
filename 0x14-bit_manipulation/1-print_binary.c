91e3a2a08b2af84a0896d7b56ef2e2921075d5e1
Chukwudi Egoebichi <chuksegoebichi.com>
2023-04-07 08:25:28 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * print_binary - to print the binary representation of a numnber
@@ -6,10 +6,18 @@
   * number to be represented to base of two
   * Return: no return
   */
-
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	(n & 1) ? _putchar('1') : _putchar('0');
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
+
+
