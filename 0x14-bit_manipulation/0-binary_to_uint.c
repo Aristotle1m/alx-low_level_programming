0f58faa5c850b2902cd5d8dd181959b545194b82
Chukwudi Egoebichi <chuksegoebichi.com>
2023-04-07 07:24:11 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,20 +5,16 @@
  * @b: binary nos
  * Return: Always (0) for unsigned int.
  */
-
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int x, y;
 	int base_two;
 
-		if (!b)
-			return (0);
-
-	x = 0;
+	if (!b)
+		return (0);
 
 	for (base_two = 0; b[base_two]; base_two++)
 		;
-
 	base_two--;
 	for (y = 1, x = 0; base_two >= 0; base_two--)
 	{
@@ -37,4 +33,3 @@ unsigned int binary_to_unit(const char *b)
 	}
 	return (x);
 }
-
