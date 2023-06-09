6e41c77f1123552980be97353ce59ebb14b8b4b0
Chukwudi Egoebichi <chuksegoebichi.com>
2023-06-09 10:57:49 -0700

    tough but gpt through

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,41 +1,65 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table
- * @key: the key (can't be empty string)
- * @value: value associated with the key (it can be empty string)
- * Return: 1 on success 0 otherwise
+ * add_n_hash - adds a node at the beginning of a hash at a given index
+ *
+ * @head: head of the hash linked list
+ * @key: key of the hash
+ * @value: value to store
+ * Return: head of the hash
  */
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *add_n_hash(hash_node_t **head, const char *key, const char *value)
 {
-	int idx;
-	hash_node_t *node;
+	hash_node_t *tmp;
 
-	if (key == NULL || strlen(key) == 0 || ht == NULL)
-		return (0);
+	tmp = *head;
 
-	idx = key_index((const unsigned char *) key, ht->size);
-
-	node = ht->array[idx];
-	while (node != NULL)
+	while (tmp != NULL)
 	{
-		if (strcmp(node->key, key) == 0)
+		if (strcmp(key, tmp->key) == 0)
 		{
-			free(node->value);
-			node->value = strdup(value);
-			return (1);
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (*head);
 		}
-		node = node->next;
+		tmp = tmp->next;
 	}
 
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+	tmp = malloc(sizeof(hash_node_t));
+
+	if (tmp == NULL)
+		return (NULL);
+
+	tmp->key = strdup(key);
+	tmp->value = strdup(value);
+	tmp->next = *head;
+	*head = tmp;
+
+	return (*head);
+}
+
+/**
+ * hash_table_set - adds a hash (key, value) to a given hash table
+ *
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * @value: value to store
+ * Return: 1 if successes, 0 if fails
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int k_index;
+
+	if (ht == NULL)
 		return (0);
-	node->key = strdup(key);
-	node->value = strdup(value);
-	node->next = ht->array[idx];
-	ht->array[idx] = node;
+
+	if (key == NULL || *key == '\0')
+		return (0);
+
+	k_index = key_index((unsigned char *)key, ht->size);
+
+	if (add_n_hash(&(ht->array[k_index]), key, value) == NULL)
+		return (0);
+
 	return (1);
 }
